535c757dc920998677a4656fd56c29a33ddd2b60
ewenb <eboisrame@outlook.com>
2019-09-10 18:46:51 +0200

    ajout BehaviorSubject + addUser

--- src/app/shared/user.service.ts
+++ src/app/shared/user.service.ts
@@ -1,9 +1,14 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+
+  public users: BehaviorSubject<string[]> = new BehaviorSubject([  ]);
+
+  addUser(): void { }
 
   constructor() { }
 }
